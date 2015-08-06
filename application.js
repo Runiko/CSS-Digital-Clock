6c430960138c71b968d9bb61a46309714fcd5c66
TravisL12 <travis.lawrence12@gmail.com>
2015-08-05 22:41:30 -0700

    Add transitions

--- application.js
+++ application.js
@@ -2,7 +2,7 @@ var secondOnes = document.getElementById("seconds-ones");
 var secondTens = document.getElementById("seconds-tens");
 
 var num = 0;
-var inter = setInterval(changeTime, 250);
+var inter = setInterval(changeTime, 1000);
 
 function changeTime() {
 	var timeClass = 'time-' + num;
