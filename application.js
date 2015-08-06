cd89ec83c83c0799101e595ba9a123e46636af95
TravisL12 <travis.lawrence12@gmail.com>
2015-08-05 22:27:04 -0700

    Initial commit

---
+++ application.js
@@ -0,0 +1,12 @@
+var secondOnes = document.getElementById("seconds-ones");
+var secondTens = document.getElementById("seconds-tens");
+
+var num = 0;
+var inter = setInterval(changeTime, 250);
+
+function changeTime() {
+	var timeClass = 'time-' + num;
+  secondOnes.className = timeClass;
+  secondTens.className = timeClass;
+	num === 9 ? num = 0 : num++;
+}
