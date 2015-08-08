c87388a18d40db31c784da7b6f1ee8e2ecef52b8
Travis Lawrence <travis.lawrence12@gmail.com>
2015-08-08 09:27:48 -0700

    Set time correctly

--- application.js
+++ application.js
@@ -1,12 +1,45 @@
-var secondOnes = document.getElementById("seconds-ones");
-var secondTens = document.getElementById("seconds-tens");
+// Seconds
+var secondOnes  = document.getElementById("seconds-ones");
+var secondTens  = document.getElementById("seconds-tens");
+// Minutes
+var minutesOnes = document.getElementById('minutes-ones');
+var minutesTens = document.getElementById('minutes-tens');
+// Hours
+var hoursOnes   = document.getElementById('hours-ones');
+var hoursTens   = document.getElementById('hours-tens');
 
-var num = 0;
+var date = new Date();
+
+function singleDigit(time) {
+	return time.length == 1 ? '0' + time : time;
+}
+
+function splitOnes(time) {
+	return singleDigit(time.toString()).split('')[1];
+}
+
+function splitTens(time) {
+	return singleDigit(time.toString()).split('')[0];
+}
+
+function setTime(date) {
+	secondOnes.className  = 'time-' + splitOnes(date.getSeconds());
+	secondTens.className  = 'time-' + splitTens(date.getSeconds());
+
+	minutesOnes.className = 'time-' + splitOnes(date.getMinutes());
+	minutesTens.className = 'time-' + splitTens(date.getMinutes());
+
+	hoursOnes.className   = 'time-' + splitOnes(date.getHours());
+	hoursTens.className   = 'time-' + splitTens(date.getHours());
+	return splitOnes(date.getSeconds());
+}
+
+
+var num = setTime(date);
 var inter = setInterval(changeTime, 1000);
 
 function changeTime() {
 	var timeClass = 'time-' + num;
-  secondOnes.className = timeClass;
-  secondTens.className = timeClass;
+  	secondOnes.className = timeClass;
 	num === 9 ? num = 0 : num++;
 }
