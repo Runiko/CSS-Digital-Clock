58356e56a427f1a1d4745e6b97d1fc3c81eb9285
Travis Lawrence <travis.lawrence12@gmail.com>
2015-08-08 16:05:33 -0700

    Now with more hex background colors!

--- application.js
+++ application.js
@@ -30,13 +30,13 @@ Digit.prototype = {
 	}
 }
 
-var date = new Date();
-
-var seconds = new Digit('seconds', 60, date.getSeconds());
-var minutes = new Digit('minutes', 60, date.getMinutes());
-var hours   = new Digit('hours',   12, date.getHours() - 12);
+function changeBackground() {
+	var hex = '#' + hours.splitTime().join('') + minutes.splitTime().join('') + seconds.splitTime().join('');
+	body.style.background = hex;
+}
 
 function checkTimes() {
+	changeBackground();
 	if (seconds.changeTime() == 0) {
 		if (minutes.changeTime() == 0) {
 			hours.changeTime();
@@ -51,4 +51,11 @@ function init() {
 
 	setInterval(checkTimes, 1000);
 }
+
+var date    = new Date();
+var seconds = new Digit('seconds', 60, date.getSeconds());
+var minutes = new Digit('minutes', 60, date.getMinutes());
+var hours   = new Digit('hours',   12, date.getHours() - 12);
+var body    = document.getElementById('body');
+
 init();
