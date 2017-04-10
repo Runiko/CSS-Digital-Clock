6bf51c9228b068027c2ecf9aa67424d62d823fa4
Travis Lawrence <tlawrence@tumblr.com>
2017-04-09 22:16:45 -0700

    Trim down sass

--- application.js
+++ application.js
@@ -1,15 +1,16 @@
-var Digit = function(el){
+let militaryTime = true;
+
+let Digit = function(el){
 	this.onesEl = document.getElementById(el + '-ones');
 	this.tensEl = document.getElementById(el + '-tens');
 }
 
 Digit.prototype = {
-
-	zeroPad: function () {
+	zeroPad () {
 		return ('0' + this.time.toString()).slice(-2);
 	},
 
-	render: function(time) {
+	render (time) {
 		this.time = time;
 		this.onesEl.className = 'time-' + this.zeroPad()[1];
 		this.tensEl.className = 'time-' + this.zeroPad()[0];
@@ -20,18 +21,23 @@ function hexTimeBackground() {
 	return '#' + hours.zeroPad() + minutes.zeroPad() + seconds.zeroPad();
 }
 
+function checkHour(date) {
+	let currentHour = date.getHours()
+	return militaryTime && (currentHour > 12) ? (currentHour - 12) : currentHour;
+}
+
 function init() {
-	var date = new Date();
+	let date = new Date();
 	seconds.render(date.getSeconds());
 	minutes.render(date.getMinutes());
-	hours.render(date.getHours() - 12);
+	hours.render(checkHour(date));
 	body.style.background = hexTimeBackground();
 	setTimeout(init, 1000);
 }
 
-var seconds = new Digit('seconds');
-var minutes = new Digit('minutes');
-var hours   = new Digit('hours');
-var body    = document.getElementById('body');
+let seconds = new Digit('seconds');
+let minutes = new Digit('minutes');
+let hours   = new Digit('hours');
+let body    = document.getElementById('body');
 
 init();
